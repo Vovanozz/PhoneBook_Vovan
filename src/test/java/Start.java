871a6581553dc32041b4eb2b10dedf2ce4232c9d
VovanOzz <120048518+Vovanozz@users.noreply.github.com>
2023-01-21 10:44:21 +0200

    myFirstProject

---
+++ src/test/java/Start.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+    public class Start {
+
+    WebDriver wd;
+    @Test
+    public void myTest(){
+        //open browser
+
+    }
+    @Test
+    public void secondTest(){
+
+
+    }
+}
